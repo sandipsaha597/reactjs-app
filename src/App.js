e4d8f81e0e001dbc9090d5f2c630c2bb8a6e499a
sandip-saha <>
2022-08-25 20:24:04 +0530

    calling api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    console.log('run')
+    fetch('api/sam')
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {count}
+        <button onClick={() => setCount((prev) => prev + 1)}>add</button>
         <p>ReactJS application in AWS EC2... {process.env.NODE_ENV}</p>
         <a
           className="App-link"
